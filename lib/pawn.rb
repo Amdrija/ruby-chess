66edf356b5f90a1dd00e0134f09f1f10dffbbf6c
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2019-09-15 18:09:18 +0200

    Add Pawn class, pawn.rb

---
+++ lib/pawn.rb
@@ -0,0 +1,63 @@
+require "chess_piece.rb"
+class Pawn < ChessPiece
+  attr_reader :opening_move
+
+  def initialize(square, white = true)
+    unicode = white ? "\u2659" : "\u265f"
+    super(square, unicode, white)
+    @opening_move = true
+  end
+
+  # returns the eaten chess piece, otherwise nil
+  def move(new_square)
+    @opening_move = false if opening_move
+    super(new_square)
+  end
+
+  def move_valid?(new_square)
+
+    horizontal_move?(new_square) || opening_move?(new_square) || eating_move?(new_square)
+
+    # TODO: add move for en_passant
+  end
+
+  private
+  def eating_move?(new_square)
+    new_square.occupied_by_different_color?(self) && diagonal_move?(new_square)
+  end
+
+  # diagonal eating move,if the pawn is white, it can go upwards and left/right
+  # otherwise,if the pawn is black, it can go downwards and left/right
+  def diagonal_move?(new_square)
+    if @white
+      return  (new_square.position[:x] - @square.position[:x]).abs == 1 &&
+              new_square.position[:y] - @square.position[:y] == 1
+    end
+
+    (new_square.position[:x] - @square.position[:x]).abs == 1 &&
+        new_square.position[:y] - @square.position[:y] == -1
+  end
+
+  def horizontal_move?(new_square)
+    return false if new_square.occupied?
+
+    if @white
+      return new_square.position[:x] == @square.position[:x] &&
+          new_square.position[:y] == @square.position[:y] + 1
+    end
+
+    new_square.position[:x] == @square.position[:x] &&
+        new_square.position[:y] == @square.position[:y] - 1
+  end
+
+  def opening_move?(new_square)
+    return false if !@opening_move
+    if @white
+      return new_square.position[:x] == @square.position[:x] &&
+          new_square.position[:y] == @square.position[:y] + 2
+    end
+
+    new_square.position[:x] == @square.position[:x] &&
+        new_square.position[:y] == @square.position[:y] - 2
+  end
+end
---
+++ spec/pawn_spec.rb
@@ -0,0 +1,143 @@
+require 'pawn.rb'
+
+RSpec.describe Pawn do
+  describe "#move(new_square)" do
+    it "Returns nil if the 0,0 square isn't occupied and moves the pawn to that square" do
+      square_start = instance_double('Square', position: { x: 0, y: 0 }, piece: nil, white: true)
+      allow(square_start).to receive(:occupy).and_return(nil)
+      square_end = instance_double('Square', position: { x: 0, y: 1 }, piece: nil, white: true)
+      allow(square_end).to receive(:occupy).and_return(nil)
+      pawn = Pawn.new(square_start)
+      expect(pawn.move(square_end)).to eql(nil)
+      expect(pawn.square.position).to eql({ x: 0, y: 1 })
+      expect(pawn.opening_move).to eql(false)
+    end
+  end
+
+  describe "#move_valid?(new_square)" do
+    it "Returns true if the white pawn can move from 0,0 to 0,1" do
+      square = instance_double("Square", position: { x: 0, y: 0})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 0, y: 1})
+      allow(new_square).to receive(:occupied?).and_return(false)
+      pawn = Pawn.new(square)
+      expect(pawn.move_valid?(new_square)).to eql(true)
+    end
+
+    it "Returns true if the white pawn can move from 2,2 to 2,3" do
+      square = instance_double("Square", position: { x: 2, y: 2})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 2, y: 3})
+      allow(new_square).to receive(:occupied?).and_return(false)
+      pawn = Pawn.new(square)
+      expect(pawn.move_valid?(new_square)).to eql(true)
+    end
+
+    it "Returns true if the black pawn can move from 2,2 to 2,1" do
+      square = instance_double("Square", position: { x: 2, y: 2})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 2, y: 1})
+      allow(new_square).to receive(:occupied?).and_return(false)
+      pawn = Pawn.new(square, false)
+      expect(pawn.move_valid?(new_square)).to eql(true)
+    end
+
+    it "Returns true if the white pawn can move from 1,1 to 1,3 as an opening move" do
+      square = instance_double("Square", position: { x: 1, y: 1})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 1, y: 3})
+      allow(new_square).to receive(:occupied?).and_return(false)
+      pawn = Pawn.new(square)
+      expect(pawn.move_valid?(new_square)).to eql(true)
+    end
+
+    it "Returns true if the black pawn can move from 3,3 to 3,1 as an opening move" do
+      square = instance_double("Square", position: { x: 3, y: 3})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 3, y: 1})
+      allow(new_square).to receive(:occupied?).and_return(false)
+      pawn = Pawn.new(square, false)
+      expect(pawn.move_valid?(new_square)).to eql(true)
+    end
+
+    it "Returns true if the white pawn can move from 2,2 to 3,3 and eat a black pawn" do
+      square = instance_double("Square", position: { x: 2, y: 2})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 3, y: 3})
+      allow(new_square).to receive(:occupied?).and_return(true)
+      allow(new_square).to receive(:occupied_by_different_color?).and_return(true)
+      pawn = Pawn.new(square)
+      expect(pawn.move_valid?(new_square)).to eql(true)
+    end
+
+    it "Returns true if the white pawn can move from 2,2 to 3,1 and eat a black pawn" do
+      square = instance_double("Square", position: { x: 2, y: 2})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 1, y: 3})
+      allow(new_square).to receive(:occupied?).and_return(true)
+      allow(new_square).to receive(:occupied_by_different_color?).and_return(true)
+      pawn = Pawn.new(square)
+      expect(pawn.move_valid?(new_square)).to eql(true)
+    end
+
+    it "Returns true if the black pawn can move from 2,2 to 3,1 and eat a white pawn" do
+      square = instance_double("Square", position: { x: 2, y: 2})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 3, y: 1})
+      allow(new_square).to receive(:occupied?).and_return(true)
+      allow(new_square).to receive(:occupied_by_different_color?).and_return(true)
+      pawn = Pawn.new(square, false)
+      expect(pawn.move_valid?(new_square)).to eql(true)
+    end
+
+    it "Returns true if the black pawn can move from 2,2 to 1,1 and eat a white pawn" do
+      square = instance_double("Square", position: { x: 2, y: 2})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 1, y: 1})
+      allow(new_square).to receive(:occupied?).and_return(true)
+      allow(new_square).to receive(:occupied_by_different_color?).and_return(true)
+      pawn = Pawn.new(square, false)
+      expect(pawn.move_valid?(new_square)).to eql(true)
+    end
+
+    it "Returns false if the white pawn can't move from 0,0 to 0,1, because 0,1 is occupied" do
+      square = instance_double("Square", position: { x: 0, y: 0})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 0, y: 1})
+      allow(new_square).to receive(:occupied?).and_return(true)
+      allow(new_square).to receive(:occupied_by_different_color?).and_return(false)
+      pawn = Pawn.new(square)
+      expect(pawn.move_valid?(new_square)).to eql(false)
+    end
+
+    it "Returns false if the white pawn can't move from 0,0 to 0,3" do
+      square = instance_double("Square", position: { x: 0, y: 0})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 0, y: 3})
+      allow(new_square).to receive(:occupied?).and_return(false)
+      allow(new_square).to receive(:occupied_by_different_color?).and_return(true)
+      pawn = Pawn.new(square)
+      expect(pawn.move_valid?(new_square)).to eql(false)
+    end
+
+    it "Returns false if the white pawn can't move from 1,1 to 1,0" do
+      square = instance_double("Square", position: { x: 0, y: 0})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 1, y: 0})
+      allow(new_square).to receive(:occupied?).and_return(false)
+      allow(new_square).to receive(:occupied_by_different_color?).and_return(true)
+      pawn = Pawn.new(square)
+      expect(pawn.move_valid?(new_square)).to eql(false)
+    end
+
+    it "Returns false if the white pawn can't move from 0,0 to 1,1, because 1,1 isn't occupied" do
+      square = instance_double("Square", position: { x: 0, y: 0})
+      allow(square).to receive(:occupy).and_return(nil)
+      new_square = instance_double("Square", position: {x: 1, y: 1})
+      allow(new_square).to receive(:occupied?).and_return(false)
+      allow(new_square).to receive(:occupied_by_different_color?).and_return(false)
+      pawn = Pawn.new(square)
+      expect(pawn.move_valid?(new_square)).to eql(false)
+    end
+  end
+end
