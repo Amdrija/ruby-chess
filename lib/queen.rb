d02cf558411393e3d60f37aabb3fc81fc5c0d21f
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2019-09-17 18:47:46 +0200

    Add Queen class

---
+++ lib/queen.rb
@@ -0,0 +1,14 @@
+require 'chess_piece.rb'
+
+class Queen < ChessPiece
+  def initialize(square, white = true)
+    unicode = white ? '\u2655' : '\u265b'
+    super(square, unicode, white)
+  end
+
+  def move_valid?(new_square)
+    x = new_square.position[:x]
+    y = new_square.position[:y]
+    horizontal_move?(y) || vertical_move?(x) || diagonal_move?(x, y)
+  end
+end
---
+++ spec/queen_spec.rb
@@ -0,0 +1,69 @@
+require 'queen.rb'
+
+RSpec.describe Queen do
+  describe "#move_valid?(new_square)" do
+    it 'Return true if the queen can move from 4,4 to 4,7' do
+      square_start = instance_double('Square', position: { x: 4, y: 4 })
+      allow(square_start).to receive(:occupy).and_return(nil)
+      square_end = instance_double('Square', position: { x: 4, y: 7 })
+      queen = Queen.new(square_start)
+      expect(queen.move_valid?(square_end)).to eql(true)
+    end
+
+    it 'Return true if the queen can move from 4,4 to 4,0' do
+      square_start = instance_double('Square', position: { x: 4, y: 4 })
+      allow(square_start).to receive(:occupy).and_return(nil)
+      square_end = instance_double('Square', position: { x: 4, y: 0 })
+      queen = Queen.new(square_start)
+      expect(queen.move_valid?(square_end)).to eql(true)
+    end
+
+    it 'Return true if the queen can move from 4,4 to 7,4' do
+      square_start = instance_double('Square', position: { x: 4, y: 4 })
+      allow(square_start).to receive(:occupy).and_return(nil)
+      square_end = instance_double('Square', position: { x: 7, y: 4 })
+      queen = Queen.new(square_start)
+      expect(queen.move_valid?(square_end)).to eql(true)
+    end
+
+    it 'Return true if the queen can move from 4,4 to 2,4' do
+      square_start = instance_double('Square', position: { x: 4, y: 4 })
+      allow(square_start).to receive(:occupy).and_return(nil)
+      square_end = instance_double('Square', position: { x: 2, y: 4 })
+      queen = Queen.new(square_start)
+      expect(queen.move_valid?(square_end)).to eql(true)
+    end
+
+    it 'Return true if the queen can move from 4,4 to 7,7' do
+      square_start = instance_double('Square', position: { x: 4, y: 4 })
+      allow(square_start).to receive(:occupy).and_return(nil)
+      square_end = instance_double('Square', position: { x: 7, y: 7 })
+      queen = Queen.new(square_start)
+      expect(queen.move_valid?(square_end)).to eql(true)
+    end
+
+    it 'Return true if the queen can move from 4,4 to 3,3' do
+      square_start = instance_double('Square', position: { x: 4, y: 4 })
+      allow(square_start).to receive(:occupy).and_return(nil)
+      square_end = instance_double('Square', position: { x: 3, y: 3 })
+      queen = Queen.new(square_start)
+      expect(queen.move_valid?(square_end)).to eql(true)
+    end
+
+    it 'Return true if the queen can move from 4,4 to 2,6' do
+      square_start = instance_double('Square', position: { x: 4, y: 4 })
+      allow(square_start).to receive(:occupy).and_return(nil)
+      square_end = instance_double('Square', position: { x: 2, y: 6 })
+      queen = Queen.new(square_start)
+      expect(queen.move_valid?(square_end)).to eql(true)
+    end
+
+    it 'Return true if the queen can move from 4,4 to 7,1' do
+      square_start = instance_double('Square', position: { x: 4, y: 4 })
+      allow(square_start).to receive(:occupy).and_return(nil)
+      square_end = instance_double('Square', position: { x: 7, y: 1 })
+      queen = Queen.new(square_start)
+      expect(queen.move_valid?(square_end)).to eql(true)
+    end
+  end
+end
